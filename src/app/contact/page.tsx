db0d3c610be23d13607c68cf61a08a803a1a0119
Khalid147-alt <khalidunar103@gmail.com>
2024-12-07 23:58:32 +0500

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>Contact</h1>
+        <p>Phone: 123-456-7890</p>
+        
+        
+      
+    </div>
+  )
+}
+
+export default Contact
