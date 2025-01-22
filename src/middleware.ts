db7077e3bf67f7a9281b360ae867582ac1af7f5f
Khalid147-alt <khalidunar103@gmail.com>
2025-01-22 15:05:03 +0500

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // This regex matches all paths except Next.js internals, certain static files, and pages found in search params.
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    // Always apply middleware for API routes
     '/(api|trpc)(.*)',
   ],
 };
-
